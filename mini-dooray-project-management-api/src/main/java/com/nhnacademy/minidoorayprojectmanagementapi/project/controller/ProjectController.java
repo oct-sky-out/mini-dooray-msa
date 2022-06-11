bb342f0df92a076f914710b513d063986eab892f
oct-sky-out <kms3335k@naver.com>
2022-06-12 03:13:04 +0900

    프로젝트 상태수정 컨트롤러 테스트완료

--- mini-dooray-project-management-api/src/main/java/com/nhnacademy/minidoorayprojectmanagementapi/project/controller/ProjectController.java
+++ mini-dooray-project-management-api/src/main/java/com/nhnacademy/minidoorayprojectmanagementapi/project/controller/ProjectController.java
@@ -2,10 +2,12 @@ package com.nhnacademy.minidoorayprojectmanagementapi.project.controller;
 
 import com.nhnacademy.minidoorayprojectmanagementapi.project.dto.CreationProjectRequest;
 import com.nhnacademy.minidoorayprojectmanagementapi.project.dto.ProjectExecutionCompleteDto;
+import com.nhnacademy.minidoorayprojectmanagementapi.project.dto.ProjectStatusModifyRequest;
 import com.nhnacademy.minidoorayprojectmanagementapi.project.service.ProjectService;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +27,11 @@ public class ProjectController {
     @ResponseStatus(HttpStatus.CREATED)
     public Map<String, Object> createProject(@Validated @RequestBody CreationProjectRequest projectRequest){
         return projectService.createProject(projectRequest);
+    }
+
+    @PatchMapping("/{id}/status")
+    public ProjectExecutionCompleteDto modifyProjectStatus(@Validated @RequestBody
+                                                           ProjectStatusModifyRequest modifyRequest){
+        return projectService.modifyProjectStatus(modifyRequest);
     }
 }
--- mini-dooray-project-management-api/src/test/java/com/nhnacademy/minidoorayprojectmanagementapi/project/controller/ProjectControllerTest.java
+++ mini-dooray-project-management-api/src/test/java/com/nhnacademy/minidoorayprojectmanagementapi/project/controller/ProjectControllerTest.java
@@ -2,6 +2,9 @@ package com.nhnacademy.minidoorayprojectmanagementapi.project.controller;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -9,6 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.minidoorayprojectmanagementapi.project.dto.CreationProjectRequest;
 import com.nhnacademy.minidoorayprojectmanagementapi.project.dto.ProjectExecutionCompleteDto;
+import com.nhnacademy.minidoorayprojectmanagementapi.project.dto.ProjectStatusModifyRequest;
 import com.nhnacademy.minidoorayprojectmanagementapi.project.entity.ProjectStatus;
 import com.nhnacademy.minidoorayprojectmanagementapi.project.service.ProjectService;
 import com.nhnacademy.minidoorayprojectmanagementapi.projectmember.dto.ProjectMemberDto;
@@ -64,5 +68,36 @@ class ProjectControllerTest {
             .andExpect(jsonPath("$.admin.userId").value(equalTo(admin.getUserId())))
             .andExpect(jsonPath("$.project.status").value(equalTo(projectExecutionCompleteDto.getStatus().getStatus())))
             .andExpect(jsonPath("$.project.name").value(equalTo(projectExecutionCompleteDto.getName())));
+    }
+
+
+    @Test
+    void modifyProjectStatusTest() throws Exception {
+        ProjectStatusModifyRequest modifyRequest = new ProjectStatusModifyRequest();
+        modifyRequest.setProjectStatus(ProjectStatus.DORMANT);
+        modifyRequest.setProjectNo(7L);
+
+        String requestBody = new ObjectMapper().writeValueAsString(modifyRequest);
+
+        ProjectExecutionCompleteDto result = new ProjectExecutionCompleteDto(
+            modifyRequest.getProjectNo(),
+            "project",
+            ProjectStatus.DORMANT,
+            LocalDateTime.now()
+        );
+
+        given(projectService.modifyProjectStatus(modifyRequest))
+            .willReturn(result);
+
+        mockMvc.perform(patch("/projects/{id}/status", 7L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.projectNo")
+                .value(equalTo(modifyRequest.getProjectNo().intValue())))
+            .andExpect(jsonPath("$.status")
+                .value(equalTo(modifyRequest.getProjectStatus().getStatus())));
+
+        verify(projectService, times(1)).modifyProjectStatus(modifyRequest);
     }
 }
