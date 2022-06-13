5e19596aa841391ebe93546c3b6bb88655c1fba9
oct-sky-out <kms3335k@naver.com>
2022-06-14 02:10:58 +0900

    내 프로젝트 목록(페이징) 샘플 http파일 추가, 목록 날짜 포맷

--- mini-dooray-project-management-api/src/main/java/com/nhnacademy/minidoorayprojectmanagementapi/project/dto/ProjectBasicDto.java
+++ mini-dooray-project-management-api/src/main/java/com/nhnacademy/minidoorayprojectmanagementapi/project/dto/ProjectBasicDto.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.minidoorayprojectmanagementapi.project.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.nhnacademy.minidoorayprojectmanagementapi.project.entity.ProjectStatus;
 import java.time.LocalDateTime;
 import lombok.Data;
@@ -15,5 +16,6 @@ public class ProjectBasicDto {
 
     private ProjectStatus status;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 }
